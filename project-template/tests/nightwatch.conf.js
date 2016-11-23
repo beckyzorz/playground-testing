1435797051f421a3e6d2ae6bfccdae20da74ed1c
beckyzorz <beckydefoe@gmail.com>
2016-11-23 23:56:11 +0000

    Added tests folder for nightwatch.js tests. Added root project directory as template for new test projects.

---
+++ project-template/tests/nightwatch.conf.js
@@ -0,0 +1,65 @@
+/*
+-http://nightwatchjs.org/getingstarted
+*/
+
+
+{
+
+/*
+Basic settings
+*/
+  "src_folders" : ["tests"],
+  "output_folder" : "reports",
+  "custom_commands_path" : "",
+  "custom_assertions_path" : "",
+  "page_objects_path" : "",
+  "globals_path" : "",
+/*
+Selenium settings
+*/
+  "selenium" : {
+    "start_process" : false,
+    "server_path" : "",
+    "log_path" : "",
+    "port" : 4444,
+    "cli_args" : {
+      "webdriver.chrome.driver" : "",
+      "webdriver.gecko.driver" : "",
+      "webdriver.edge.driver" : ""
+    }
+  },
+/*
+Test settings
+
+ Launch environments with their key e.g. nightwatch --env chrome. CONTINUE FROM TEST GLOBALS.
+
+*/
+  "test_settings" : { /* Default environment */
+    "default" : {
+      "launch_url" : "http://localhost", /*  CHANGE ME  */
+      "selenium_port"  : 4444,
+      "selenium_host"  : "localhost",
+      "silent": true,
+      "screenshots" : {
+        "enabled" : false,
+        "path" : ""
+      },
+      "desiredCapabilities": {
+        "browserName": "firefox",
+        "marionette": true
+      }
+    },
+
+    "chrome" : {
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    },
+
+    "edge" : {
+      "desiredCapabilities": {
+        "browserName": "MicrosoftEdge"
+      }
+    }
+  }
+}
